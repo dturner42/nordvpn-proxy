5bacc3cd726701874edb21754a33a239f1e729ad
rootEth <turner.danieljohn@gmail.com>
2022-02-08 01:29:32 +0000

    fix scope of filename

--- app/ovpn/get-ovpn-files.sh
+++ app/ovpn/get-ovpn-files.sh
@@ -3,19 +3,20 @@
 . /app/date.sh --source-only
 nordvpn_hostname=$(cat /tmp/nordvpn_hostname)
 
+#Set file name
+if [ "${PROTOCOL,,}" = "udp" ]; then
+    filename=$nordvpn_hostname${OVPN_UDP}
+else
+    filename=$nordvpn_hostname${OVPN_TCP}
+fi
+
+
 function download_files {
   # the current ovpn zip file is more than two hours old.
   echo "$(adddate) INFO: Downloading new OVPN file."
 
   #Create directory if no volume is done
   mkdir -p ${OVPN_CONFIG_DIR}
-
-  #Set file name
-  if [ "${PROTOCOL,,}" = "udp" ]; then
-      filename=$nordvpn_hostname${OVPN_UDP}
-  else
-      filename=$nordvpn_hostname${OVPN_TCP}
-  fi
 
   #Remove current zip
   rm -rf ${OVPN_CONFIG_DIR}/$filename
